51bdb725047420e46a3c3c6b3be61ce8d554f8b4
denjukebox <stijn@adsj.be>
2020-06-22 08:54:59 +0200

    Add CompilationScript to make ISO images from folder structure

---
+++ CompilationScript.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Name of device recovery image for resources folderDEFAULT Surface_RT1
+DEVICE_NAME=$1
+# Version of windows to use
+WIN_VERSION=$2
+# Type of image for device in device recovery fs folder
+IMAGE_VARIANT=$3
+#Compound image name & location
+IMAGE_NAME="${DEVICE_NAME}_WinRT_${WIN_VERSION}_${IMAGE_VARIANT}"
+IMAGE_LOCATION="ISO/${IMAGE_NAME}.iso"
+
+LODEV=$(sudo losetup -f)
+fallocate -l 4G ${IMAGE_LOCATION}
+sudo -s <<EOF
+losetup --partscan --show --find "${IMAGE_LOCATION}"
+(echo o; echo n; echo p; echo 1; echo ""; echo ""; echo "t"; echo "b"; echo w; echo q) | fdisk $(echo $LODEV)
+mkfs.vfat -n "RECOVERY" -M 0xF9 -v "${LODEV}p1"
+mount "${LODEV}p1" mount
+unzip "Resources/RecoveryFS/${DEVICE_NAME}/WinRT_80_${IMAGE_VARIANT}.zip" -d mount
+cp -r -v Resources/Windows/SecureBoot mount
+cp -r -v Resources/Windows/EFIESP mount
+cp -r -v Resources/Linux/GRUB mount
+cp -r -v Scripts mount
+umount mount
+losetup -d $(echo $LODEV)
+EOF
+exit 0
